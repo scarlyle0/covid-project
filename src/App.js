949ea8168d7a80d7e78d2f3f56c134a189e6e917
Sean Carlyle <808jalapeno@gmail.com>
2022-05-16 00:34:36 -0700

    adding/deleting files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [boool, setBoool] = useState(true)
+  const name = "yes"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {boool ? name : "not true"}
+      <div
+      onClick={() => setBoool(!boool)}>
+        <h1>changeBool </h1>
+      </div>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            I am the best
+        </div>
+    );
+}
+
+export default Navbar;
