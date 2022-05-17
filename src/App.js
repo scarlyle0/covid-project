eb2ac3127a639282823eb5e1eb6ab13098196e40
Sean Carlyle <808jalapeno@gmail.com>
2022-05-17 00:15:25 -0700

    adding files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-//import React, { useState } from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
+import './index.css'
+import Main from "./components/Main";
+import Dropdown from "./components/Dropdown";
 
 function App() {
+  const[selected, setSelected] = useState('Global')
+  const[open, setOpen] = useState(false)
 
   return (
     <div className="App">
-
-      <Navbar/>
+      
+      <Navbar selected = {selected} setSelected = {setSelected}
+      open = {open} setOpen={setOpen}/>
+      <main className="Main">
+        {open ?
+        <Dropdown/> :
+        <Main/>
+        }
+      </main>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,29 @@
-import React from 'react';
-//import styles from './navbar.module.css'
+import React, {useState} from 'react';
+import styles from './navbar.module.css'
 import useFetchData from '../hooks/fetchData';
 
 
 //export default 
 function Navbar(props) {
+    const {selected, setSelected, open, setOpen} = props
 
     const countriesUrl = 'https://covid19.mathdro.id/api/countries'
     const {data, loading, error} = useFetchData(countriesUrl)
-
+    
+    console.log(open)
     if (error){
         return <div>
             ERROR
         </div>
     }
     return (
-        <div>
+        <div className={styles.navbar} onClick={() => setOpen(!open)} >
+            
             {loading ?
                 <div>Global</div> :
-                <div>data</div>
+                <div>{selected}</div>
                 }
+            <i style={{ transform: `${open ? 'rotate(180deg)' : ''}`, transitionDuration: '300ms'}} className="fa-solid fa-chevron-down"></i>
         </div>
     );
 }
