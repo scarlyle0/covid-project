33e7d775f671d3e46015e154704e78f7842f0bd8
Sean Carlyle <808jalapeno@gmail.com>
2022-05-20 00:14:09 -0700

    working on graphs css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import styles from './cases.module.css'
 import useFetchData from '../hooks/fetchData';
 
 function Main(props) {
     const { selected, data } = props
-    const cases = data[selected]
-
-    if (selected === 'Global'){
-        return <div>Global</div>
+    const cases = data[selected].All
+    const totals = {
+        confirmed: cases.confirmed,
+        recovered: cases.confirmed - cases.deaths,
+        deaths: cases.deaths
     }
 
-
     return(
-            <div>
-                Main
+            <div className={styles.graph}>
+                {Object.keys(totals).map((val, index) => {
+                    return <div key = {index} className={styles.bar}>
+                        {val}
+                    </div>
+                })}
             </div>
         )
     }
