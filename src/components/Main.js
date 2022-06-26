5c52d1bba1352a4a0f22a88a9e9a511b2ac91981
Sean Carlyle <scaryle@ucsc.edu>
2022-06-26 00:57:50 -1000

    working on graphs

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './cases.module.css'
-import useFetchData from '../hooks/fetchData';
+//import useFetchData from '../hooks/fetchData';
 
 function Main(props) {
     const { selected, data } = props
@@ -11,11 +11,17 @@ function Main(props) {
         deaths: cases.deaths
     }
 
+    const max = Math.max(...Object.values(totals))
+
     return(
             <div className={styles.graph}>
+                {[1,2,3,4,5].map(val => {
+                    return <div style={{top: `${val*20 - 20}`}} className={styles.gridLines} key={val}></div>
+                })}
                 {Object.keys(totals).map((val, index) => {
-                    return <div key = {index} className={styles.bar}>
-                        {val}
+                    return <div key = {index} className={`${styles.bar} ${styles[`bar${index}`]}`} style={
+                        {height: `${(totals[val] / max)*100 }%`}}>
+                        <p className={styles.barLabels}>{val}</p>
                     </div>
                 })}
             </div>
